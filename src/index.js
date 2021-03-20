9ea841759409227c4c9a652b3dbdd9215c6f45be
saherkh1 <45327854+saherkh1@users.noreply.github.com>
2021-03-20 18:31:03 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+// ReactDOM.render(WHAT TO SHOW,WHERE TO SHOW IT);
+ReactDOM.render(
+  <div>
+    <h1> Hello World</h1>
+    <p>this is a text</p>
+  </div>,
+  document.getElementById("root")
+);
